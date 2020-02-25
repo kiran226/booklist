f66330109968ac96d2dbaa1cba3a5bc018c42077
kiran <gandikotakiran99@gmail.com>
2020-02-25 11:11:44 +0530

    added styles and functionality to remove the book from the list

---
+++ src/components/BookList.js
@@ -0,0 +1,21 @@
+import React,{useContext} from 'react';
+import { BookContext } from '../context/BookContext';
+import Bookdetails from './Bookdetails';
+const BookList=()=>{
+    const { books }=useContext(BookContext)
+    return books.length ? (
+        <div className="book-list">
+            <ul>
+                    {books.map(book=>{
+                        return(
+                            <Bookdetails book={book} key={book.id}/>
+                        )
+                    })}
+            </ul>
+        </div>):(
+            <div className="empty">
+                No books to read, hello free time..!!
+            </div>
+        )
+}
+export default BookList;
